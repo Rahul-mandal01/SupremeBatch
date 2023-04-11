6fbd6e9ec976a8f72cdf2bd374d850db72857474
rahul.mandal01 <rahulra16809999@gmail.com>
2023-04-11 21:39:30 +0530

    updateDataTypes

---
+++ BASICS OF PROGRAMMING/A/index.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int a=5;
+    int b=3;
+
+    cout<<a+b <<endl;
+    cout<<a-b <<endl;
+    cout<<a*b <<endl;
+    cout<<a/b <<endl;
+    cout<<a%b <<endl;
+    cout<<(a<=b)<<endl;
+
+
+    if(a>=5 && b<=10)
+        cout<<"Limits Reached"<<endl;
+    else
+        cout<<"hii"<<endl;
+    // double d = 5.3;
+
+    // int x = (int)d;
+    // cout<< "value of x is:" <<x<<endl;
+
+
+    // char ch =97;
+    // cout<<ch <<endl;
+
+    // int num = 'b';
+    // cout<<num<<endl;
+    // int b=10;
+    // long long a=5;
+    // cout<<"value of a is:" <<a <<endl;
+    // cout<<"Size of a is:"<<sizeof(a)<<endl;
+
+    // char ch=256;
+    // cout<<"Value of ch is:"<<ch<<endl;
+
+    // bool flag=true;
+    // cout<<"value of flag is:" <<flag<<endl;
+
+
+
+
+
+    // cout<<"Namaste Bharat";
+    // cout<<endl;
+    // cout<<'\n';
+
+    // cout<<"Enter a Number:";
+    // int a;
+    // taking input
+    // cin>>a;
+
+    // cout<<"You Entered:" << a <<endl;
+}
