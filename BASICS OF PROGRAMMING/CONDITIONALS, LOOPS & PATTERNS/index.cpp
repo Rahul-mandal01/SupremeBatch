be1a7c02917ac9b04257f3098252ffdd7e560137
rahul.mandal01 <rahulra16809999@gmail.com>
2023-04-21 21:28:02 +0530

    updates conditionals loops and patterns

---
+++ BASICS OF PROGRAMMING/CONDITIONALS, LOOPS & PATTERNS/index.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+    // ----------CONDITIONALS-------------
+
+    // // declare a integer type variable
+    // int score;
+
+    // // print enter the score
+    // cout<<"Enter the score "<<endl;
+
+    // // take input into score variable
+    // cin>>score;
+    
+    // // if condition
+    // if(score>300)
+    //     cout<<"India wins"<<endl;
+    // else
+    //     cout<<"Pak wins"<<endl;
+
+
+    // int age;
+    // cout<<"Enter the age "<<endl;
+    // cin>>age;
+
+    // if(age<18)
+    //     cout<<"Can Vote"<<endl;
+    // else
+    //     cout<<"can not vote"<<endl;
+
+
+    // int marks;
+    // cout<<"Enter the marks: "<<endl;
+    // cin>>marks;
+
+    // if(marks>=90)
+    //     cout<<"A";
+    // else{
+    //     if(marks>=80)
+    //         cout<<"B";
+    //     else{
+    //         if(marks>=60)
+    //             cout<<"C";
+    //         else{
+    //             if(marks>=40)
+    //                 cout<<"D";
+    //             else
+    //                 cout<<"FAIL";
+    //         }
+    //     }
+    // }
+
+
+
+    // if(marks>=90)
+    //     cout<<"A ";
+    // else if(marks>=80)
+    //     cout<<"B";
+    // else if(marks>=60)
+    //     cout<<"C";
+    // else if(marks>=40)
+    //     cout<<"D";
+    // else
+    //     cout<<"FAIL";
+
+
+
+
+    // int broNum;
+    // cout<<"How many brothers are there: "<<endl;
+    // cin>>broNum;
+
+    // if(broNum==0)
+    //     cout<<"Baat Ban Jayegi"<<endl;
+    // else
+    //     cout<<"Baat nhi banegi"<<endl;
+
+
+//-------------------LOOPS-------------
+    // for(int i=0; i<5; i++){
+    //     cout<<"Rahul Love"<<endl;
+    // }
+
+    // for(int i=0; i<5; i++)
+    //     cout<<i<<endl;
+
+    // for(int i=5; i>0; i--){
+    //     cout<< i <<endl;
+    // }
+
+    // for(int i=1; i<=10; i++)
+    //     cout<< 2*i <<endl;
+
+    // for(int i=0; i<=5; i+=2)
+    //     cout<< i <<endl;
+
+    // for(int i=1; i<=5; i+=2)
+    //     cout<< i <<endl;
+
+    // for(int i=1; i<10; i*=2)
+    //     cout << i <<endl;
+
+    // for(int i=100; i>0; i/=2)
+        // cout<< i <<endl;
+
+    // for(int i=5; (i>0 && i<=10) ; i++)
+    //     cout<< i  <<endl;
+
+    // for(int i=0; i<5; i++)
+    //     cout<< i <<endl;
+
+    int i;
+    for( ; ; ){
+        if(i<5){
+            cout<< i <<endl;
+            i=i+1;
+        }
+    }
+}
---
+++ BASICS OF PROGRAMMING/CONDITIONALS, LOOPS & PATTERNS/tempCodeRunnerFile.cpp
@@ -0,0 +1,23 @@
+dl;
+    // }
+
+    // for(int i=0; i<5; i++)
+    //     cout<<i<<endl;
+
+    // for(int i=5; i>0; i--){
+    //     cout<< i <<endl;
+    // }
+
+    // for(int i=1; i<=10; i++)
+    //     cout<< 2*i <<endl;
+
+    // for(int i=0; i<=5; i+=2)
+    //     cout<< i <<endl;
+
+    // for(int i=1; i<=5; i+=2)
+    //     cout<< i <<endl;
+
+    // for(int i=1; i<10; i*=2)
+    //     cout << i <<endl;
+
+    for(int i=50; i<
