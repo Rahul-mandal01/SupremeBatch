34627defba9f9d904520781d9c8dc817869adbf5
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-08 20:51:36 +0530

    Update NUMERIC FULL PYRAMID

--- BASICS OF PROGRAMMING/ASSIGNMENT/NUMERIC FULL PYRAMID/index.cpp
+++ BASICS OF PROGRAMMING/ASSIGNMENT/NUMERIC FULL PYRAMID/index.cpp
@@ -8,18 +8,18 @@ int main(){
     for(int i=0; i<n; i++){
 
         for(int j=0; j<n-i-1; j++){
-            cout<< " ";
+            cout<< "  ";
         }
 
         for(int j=0; j<i+1; j++){
             int ans = i+1+j  ;
             
-            cout<<ans;
+            cout<<ans<<" ";
         }
 
         for(int j=i; j>0; j--){
             int ans = i+j;
-            cout<<ans;
+            cout<<ans<<" ";
 
         }
 
