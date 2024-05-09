f3c2c8642f6c6ecdff4f4f4661da65b38c908836
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-09 20:56:51 +0530

    Uploading FULL PYRAMID

--- BASICS OF PROGRAMMING/ASSIGNMENT/FULL PYRAMID/index.cpp
+++ BASICS OF PROGRAMMING/ASSIGNMENT/FULL PYRAMID/index.cpp
@@ -10,14 +10,14 @@ int main(){
         int k=0;
         for(int j=0; j< (2*n)-1 ; j++){
             if(j<n-i-1){
-                cout<<" ";
+                cout<<"  ";
             }
             else if(k<2*i+1){
-                cout<<"*";
+                cout<<"* ";
                 k++;
             }
             else{
-                cout<< " ";
+                cout<< "  ";
             }
         }
 
