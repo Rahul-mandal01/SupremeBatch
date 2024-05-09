56cec063b43d3fcde9b839af809ff99e33239a59
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-09 20:56:05 +0530

    Uploading FANCY PATTERN #1

---
+++ BASICS OF PROGRAMMING/ASSIGNMENT/FANCY PATTERN #1/index.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Please enter the number: ";
+    int n;
+    cin>>n;
+
+    cout<<" Please enter the starting number: ";
+    int m;
+    cin>>m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout<< m;
+        }
+        m++;
+
+        cout<<endl;
+    }
+
+            m--;
+
+        for(int i=0; i<n-1; i++){
+                m--;
+            for(int j=0; j<n-i-1; j++){
+                cout<<m;
+            }
+
+            cout<<endl;
+    }
+
+}
