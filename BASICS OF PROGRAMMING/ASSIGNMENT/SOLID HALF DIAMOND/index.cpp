5a2ba819ca768e979fc544adf8d1435cca7032e7
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-17 20:54:48 +0530

    Updated with optimize code

--- BASICS OF PROGRAMMING/ASSIGNMENT/SOLID HALF DIAMOND/index.cpp
+++ BASICS OF PROGRAMMING/ASSIGNMENT/SOLID HALF DIAMOND/index.cpp
@@ -6,15 +6,15 @@ int main(){
     int n;
     cin>>n;
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<i+1; j++){
-            cout<<"* ";
+    for(int i=0; i<2*n-1; i++){
+        int condition = 0;
+        if(i<n){ //Growing phase of Diamond
+            condition = i;
         }
-        cout<<endl;
-    }
-
-    for(int i=0; i<n-1; i++){
-        for(int j=n-i-1; j>0; j--){
+        else{ //Shrinking phase of Diamond
+            condition = n- (i%n) -2;
+        }
+        for(int j=0; j<=condition; j++){
             cout<<"* ";
         }
         cout<<endl;
