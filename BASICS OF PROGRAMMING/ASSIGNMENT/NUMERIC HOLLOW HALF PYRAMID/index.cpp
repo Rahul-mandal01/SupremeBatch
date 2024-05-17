7c65d61ff139eebfb0c8569f1a2606cfaa820615
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-17 20:54:29 +0530

    Updated with optimize code

--- BASICS OF PROGRAMMING/ASSIGNMENT/NUMERIC HOLLOW HALF PYRAMID/index.cpp
+++ BASICS OF PROGRAMMING/ASSIGNMENT/NUMERIC HOLLOW HALF PYRAMID/index.cpp
@@ -6,17 +6,11 @@ int main(){
     cin>>n;
 
     for(int i=0; i<n; i++){
-        int k=1;
         for(int j=0; j<=i; j++){
-            if( j==0 || i==(n-1) ){
-                cout<< k <<"  " ;
-                k++;
+            if( j==0 || j==i || i==(n-1) ){
+                cout<< j+1 <<"  " ;
             }
-
-            else if( j==i){
-                k--;
-                cout<< k+i;
-            }
+            
             else{
                 cout<<"   ";
             }
