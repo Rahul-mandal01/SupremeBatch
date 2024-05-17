3295a2ed3adb502caf01f3d30695f676133cf9cf
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-17 20:54:07 +0530

    updated with optimize code

--- BASICS OF PROGRAMMING/ASSIGNMENT/NUMERIC HOLLOW INVERTED HALF PYRAMID/index.cpp
+++ BASICS OF PROGRAMMING/ASSIGNMENT/NUMERIC HOLLOW INVERTED HALF PYRAMID/index.cpp
@@ -6,15 +6,9 @@ int main(){
     cin>>n;
 
     for(int i=0; i<n; i++){
-        int count = i+1;
-        for(int j=0; j<n-i; j++){
-            if(i==0 || j==0){
-                cout<<count<<" ";
-                count++;
-            }
-
-            else if( j == (n-i-1)){    
-                cout<<n<<" ";
+        for(int j=i+1; j<=n; j++){
+            if(j==i+1 || j==n || i==0){
+                cout<<j<<" ";
             }
             
             else{
