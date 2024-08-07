f84003f1d3aff1640e9baf3929053b57a1fce956
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-08-07 23:05:24 +0530

    Uploading VECTOR

---
+++ ARRAY/VECTOR/index.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    //----------------- creating vector 
+    // vector<int> arr;
+
+    // int ans = (sizeof(arr) / sizeof(int));
+    // cout<< ans << endl;
+
+    // cout<< arr.size() <<endl;
+    // cout<< arr.capacity() <<endl;
+
+    // ----------------insert ------------------------
+    // arr.push_back(5);
+    // arr.push_back(10);
+
+    // -----------------print ------------------------
+
+    // for(int i=0; i<arr.size(); i++){
+    //     cout<< arr[i] << " ";
+    // }
+
+    // cout<<endl;
+
+    // ------------------remove------------------------
+    // arr.pop_back();
+
+    // -----------------print ------------------------
+    // for(int i=0; i<arr.size(); i++){
+    //     cout<< arr[i] << " ";
+    // }
+
+    // cout<<endl;
+
+    // vector<int> brr(10);
+
+    // cout<<"size of b " <<brr.size()<<endl; //----size of b 10
+    // cout<<"capacity of b " <<brr.capacity()<<endl;  //----capacity of b 10
+
+    // for(int i=0; i<brr.size(); i++){
+    //     cout<< brr[i] << " ";       //---------output will be 0 0 0 0 0 0 0 0 0 0
+    // }
+
+
+    // cout<<" Enter the value of n" <<endl;
+    // int n;
+    // cin>>n;
+
+
+    // ------------------initialisation of vecotr------------------------
+    // vector<int> crr( n, 501 );
+
+    // for(int i=0; i<crr.size(); i++){
+    //     cout<<crr[i]<<" ";
+    // }
+
+
+    // -------another way to create a vector
+    // vector<int> crr{10,20,30,40,50,60};
+
+    // cout<<"size of c " <<crr.size()<<endl;
+    // cout<<"capacity of c " <<crr.capacity()<<endl;
+
+    // for(int i=0; i<crr.size(); i++){
+    //     cout<<crr[i]<<" ";
+    // }
+
+    // cout<<endl;
+
+    // cout<<"crr vector is empty or not : "<<crr.empty()<<endl;
+
+
+
+    // vector<int> drr;
+    // cout<<"drr vector is empty or not : "<< drr.empty() <<endl;
+
+
+
+
+    return 0;
+
+
+}
