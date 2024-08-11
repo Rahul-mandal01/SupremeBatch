1759d5195751700edded8714e82957cae702a0b2
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-08-11 20:02:18 +0530

    Creating 2D Array

---
+++ ARRAY/2D ARRAY/index.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    // -----------------Declare 2D Array
+    // int arr[3][3];
+
+    // ----------------- Initialize 2D Array
+    // int brr[3][3] = {   
+    //                     {1,2,3},
+    //                     {4,5,6},
+    //                     {7,8,9}
+    //                 };
+
+    // ----------------- Output in 2D Array
+
+    // cout<< brr[1][2]<<endl;
+
+    // ------------------ROW-WISE PRINTING
+
+    // outer loop
+
+    // for(int i=0; i<3; i++){
+    //     // for every row, we need to print value in each column
+    //     for(int j=0; j<3; j++){
+    //         cout<<brr[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ------------------COLUMN-WISE PRINTING
+    // outer loop
+    // for(int i=0; i<3; i++){
+    //     for(int j=0; j<3; j++){
+    //         cout<<brr[j][i]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // int arr[3][3];
+    // int rows = 3;
+    // int cols = 3;
+
+    // -------------------taking input
+
+    // cout<<"Enter the values of 2D arrays"<<endl;
+    // // row-wise
+    // for(int i=0; i<rows; i++){
+    //     for(int j=0; j<cols; j++){
+    //         cin>>arr[i][j];
+
+    //     }
+    // }
+
+
+    // cout<<"Printing row wise"<<endl;
+    // for(int i=0; i<rows; i++){
+    //     for(int j=0; j<cols; j++){
+    //         cout<<arr[i][j]<<" ";
+
+    //     }
+    //     cout<<endl;
+    // }
+
+    // cout<<"Printing col wise"<<endl;
+    // for(int i=0; i<rows; i++){
+    //     for(int j=0; j<cols; j++){
+    //         cout<<arr[j][i]<<" ";
+
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    
+    // ---------taking inut col wise
+
+    // for(int i=0; i<rows; i++){
+    //     for(int j=0; j<cols; j++){
+    //         cin>>arr[j][i];
+
+    //     }
+    // }
+
+    // cout<<"printing"<<endl;
+    // for(int i=0; i<rows; i++){
+    //     for(int j=0; j<cols; j++){
+    //         cout<<arr[i][j]<<" ";
+
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+
+
+
+    return 0;
+}   
