90d3321cedccbdb64805a515830af1f3d5241c7a
rahul <rahulra16809999@gmail.com>
2024-10-02 22:04:23 +0530

    Add solution for valid palindrome

---
+++ CHAR ARRAYS & STRINGS/ASSIGNMENT/VALID PALINDROME II/index.cpp
@@ -0,0 +1,52 @@
+// LEETCODE------680
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+ 
+
+// Example 1:
+
+// Input: s = "aba"
+// Output: true
+// Example 2:
+
+// Input: s = "abca"
+// Output: true
+// Explanation: You could delete the character 'c'.
+// Example 3:
+
+// Input: s = "abc"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of lowercase English letters.
+
+
+class Solution {
+public:
+    bool checkPalindrome(string s, int i, int j){
+        while(i <= j){
+            if(s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i= 0;
+        int j= s.length()-1;
+        while(i <= j){
+            if(s[i] != s[j]){
+                // ek baar i ko remove kr do, ek baar j ko remove kr do
+                return checkPalindrome(s, i+1 ,j) || checkPalindrome(s, i, j-1);
+            }else{
+                // s[i] == s[j]
+                i++, j--;
+            }
+        }
+        return true; //if given string pehle se hi palindrome ho, means kuchh remove na krna pde 
+    }
+};
