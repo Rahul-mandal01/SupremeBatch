3c9eea69473f991c42d6dd3d7cb267f6960c428c
rahul <rahulra16809999@gmail.com>
2024-11-06 10:13:48 +0530

    Add codes of introduction class

--- POINTERS/INTRODUCTION/index.cpp
+++ POINTERS/INTRODUCTION/index.cpp
@@ -10,21 +10,81 @@ int main(){
     // cout<< &a <<endl;
     // cout<< &b <<endl;
 
-    int a = 5;
+    // int a = 5;
 
     // POINTER CREATE
-    int *ptr = &a;  //say like this --- ptr is a pointer to a
+    // int *ptr = &a;  //say like this --- ptr is a pointer to a
 
     // ACCESS THE VALUE ptr IS POINTING TO 
-    cout << "Address of a is: " << &a <<endl;
-    cout << "Value stored at ptr is: " << ptr <<  endl;
-    cout << "Value ptr is pointing to is: " << *ptr << endl;       //--------------DEREFERENCE OPERATOR
-    cout << &ptr << endl;
+    // cout << "Address of a is: " << &a <<endl;
+    // cout << "Value stored at ptr is: " << ptr <<  endl;
+    // cout << "Value ptr is pointing to is: " << *ptr << endl;       //--------------DEREFERENCE OPERATOR
+    // cout << &ptr << endl;
     // CHANGE THE VALUE THE POINTER IS POINTING TO
 
 
+    //-------------------FIND SIZE----------
+    // int a=5;
+    // int *p = &a;
+    // cout<< sizeof(p) <<endl;
 
-    // hiiiiii
+
+    // char ch = 'b';
+    // char *c = &ch;
+    // cout<< sizeof(c) <<endl;
+
+    // double d = 1.03;
+    // double *dtr = &d;
+    // cout<< sizeof(dtr) <<endl;
+
+
+    //--------------------BAD PRACTICE------------------------ 
+    // int *ptr;
+    // cout<< ptr <<endl;
+
+    // NULL POINTER
+    // int *ptr=0;
+    // cout<< *ptr <<endl;
+
+
+    // COPY POINTER
+
+    // int a = 5;
+    // int* ptr = &a;
+
+    // int* dusraPtr = ptr;
+
+    // cout<< *ptr <<endl;
+    // cout<< *dusraPtr <<endl;
+
+
+    int a = 10;
+    int* p = &a;
+    int* q = p;
+    int* r = q;
+
+    cout << a <<endl;
+    cout << &a <<endl;
+    cout << p <<endl;
+    cout << &p <<endl;
+    cout << *p <<endl;
+    cout << q <<endl;
+    cout << &q <<endl;
+    cout << *q <<endl;
+    cout << r <<endl;
+    cout << &r <<endl;
+    cout << *r <<endl;
+    cout << (*p + *q + *r) <<endl;
+    cout <<  (*p)*2 + (*r)*3 <<endl;
+    cout << (*p/2) - (*q/2) <<endl;
+
+
+
+
+
+
+
+
 
 
     return 0;
