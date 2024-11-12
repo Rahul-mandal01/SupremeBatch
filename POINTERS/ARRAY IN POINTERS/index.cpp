220237bf9d3ec350da7a11b58f68a6ad1b53f5a8
rahul <rahulra16809999@gmail.com>
2024-11-12 18:21:40 +0530

    Add codes of int array in pointer

---
+++ POINTERS/ARRAY IN POINTERS/index.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // int arr[4] = {12, 44, 66, 18};
+
+    // cout << arr <<endl;
+    // cout << arr[0] <<endl;
+    // cout << &arr << endl;
+    // cout << &arr[0] << endl; 
+
+    // int *p = arr;
+    // cout << p << endl;
+    // cout << &p << endl;
+
+    // cout << *arr << endl;
+    // cout << arr[0] <<endl;
+    // cout << *arr + 1 << endl;
+    // cout << *(arr) + 1 << endl;
+    // cout << *(arr + 1) << endl;
+    // cout << arr[1] <<endl;
+    // cout << *(arr + 2) << endl;
+    // cout << arr[2] <<endl;
+    // cout << *(arr + 3) << endl;
+    // cout << arr[3] <<endl;
+
+    // int i = 0;
+    // cout << arr[i] << endl;
+    // cout << i[arr] << endl;
+    // cout << *(arr + i) << endl;
+    // cout << *(i + arr) <<endl;
+
+    // int arr[10];
+    // cout << sizeof(arr) << endl;
+
+    // int *p = arr;
+    // cout << sizeof(p) << endl;
+    // cout << sizeof(*p) << endl;
+
+    // char ch[10] = "Rahul";
+    // char *c = ch;
+
+    // cout << ch << endl;
+    // cout << &ch << endl;
+    // cout << ch[0] << endl;
+
+    // cout << &c << endl;
+    // cout << *c << endl;
+    // cout << c <<endl;
+
+
+    // char name[9] = "SherBano";
+    // char *c = &name[0];
+
+    // cout << name << endl;
+    // cout << &name << endl;
+    // cout << *(name+3) << endl;
+    // cout << c << endl;
+    // cout << &c << endl;
+    // cout << *(c+3) << endl;
+    // cout << (c+2) << endl;
+    // cout << *c << endl;
+    // cout << (c+8) << endl;
+
+    char ch = 'k';
+    char* cptr = &ch;
+
+    cout << cptr << endl;
+
+    return 0;
+}
