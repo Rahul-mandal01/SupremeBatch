09221ac4cecdbe6fa5153e896344946a70e76e68
rahul <rahulra16809999@gmail.com>
2024-11-13 23:11:06 +0530

    Add char array in pointer

--- POINTERS/ARRAY IN POINTERS/index.cpp
+++ POINTERS/ARRAY IN POINTERS/index.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
+
+// void solve(int arr[]){
+//     cout << "Size inside solve function " << sizeof(arr) <<endl;
+//     cout << "arr: " << arr <<endl;
+//     cout << "&arr: " << &arr <<endl;
+//     arr[0] = 50;
+// }
+
+void update(int *p){
+    cout << "Address stored inside p is: "<< p <<endl;
+    cout << "Address of p is: "<< &p << endl;
+    *p = *p +10;
+}
 int main(){
 
     // int arr[4] = {12, 44, 66, 18};
@@ -62,10 +75,45 @@ int main(){
     // cout << *c << endl;
     // cout << (c+8) << endl;
 
-    char ch = 'k';
-    char* cptr = &ch;
+    // char ch = 'k';
+    // char* cptr = &ch;
 
-    cout << cptr << endl;
+    // cout << cptr << endl;
+
+
+    // -------------------Good Practice
+    // char name[10] = "babbar";
+    // cout << name << endl;
+
+    // -------------------Bad Practice
+    // char* c = "babbar";
+    // cout << c <<endl;
+
+    // int arr[10] = {1,2,3,4};
+    // cout << "Size inside main function " << sizeof(arr) <<endl;
+    // cout << arr <<endl;
+    // cout << &arr <<endl;
+
+    // // PRINTING ARRAY INSIDE MAIN
+    // for(int i=0; i<10; i++){
+    //     cout << arr[i] << " ";
+    // }cout << endl;
+
+    // cout << endl << endl << "Now calling solve function" << endl;
+    // solve(arr);
+
+    // cout << "wapas main function me aa gya hai" << endl;
+    // for(int i=0; i<10; i++){
+    //     cout << arr[i] << " ";
+    // }cout << endl;
+
+    int a = 5;
+    cout << "Address of a is: "<< &a << endl;
+    int *ptr = &a;
+    cout << "Address stored in ptr is: "<< ptr <<endl;
+    cout << "Address of ptr is: "<< &ptr <<endl;
+    update(ptr);
+    cout << "value of a is: " << a << endl;
 
     return 0;
 }
