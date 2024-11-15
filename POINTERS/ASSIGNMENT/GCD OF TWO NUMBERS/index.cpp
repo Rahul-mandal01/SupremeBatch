a30eb5f9f9c3beda6decb9ac692f7837b2cd7da5
rahul <rahulra16809999@gmail.com>
2024-11-15 20:03:06 +0530

    Implement solution for finding GDC of two numbers

--- POINTERS/ASSIGNMENT/GCD OF TWO NUMBERS/index.cpp
+++ POINTERS/ASSIGNMENT/GCD OF TWO NUMBERS/index.cpp
@@ -29,3 +29,61 @@
 // Constraints:
 // 1 ≤ a, b ≤ 109
 
+
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        // If one of the numbers is 0, return the other number as GCD
+        if(a == 0) 
+            return b;
+        
+        if(b == 0)
+            return a;
+        
+        // Loop until one of the numbers becomes 0
+        while(a > 0 && b > 0) {
+            // Subtract the smaller number from the larger number
+            if(a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        // Return the non-zero number, which is the GCD
+        return a == 0 ? b : a;
+    }
+};
+
+// { Driver Code Starts
+// int main(){
+//     int t;
+//     // Input number of test cases
+//     scanf("%d", &t);
+//     while(t--) {
+//         int a;
+//         // Input first number
+//         scanf("%d", &a);
+        
+//         int b;
+//         // Input second number
+//         scanf("%d", &b);
+        
+//         Solution obj;
+//         // Calculate GCD of a and b
+//         int res = obj.gcd(a, b);
+        
+//         // Output the result
+//         cout << res << endl;
+        
+//         // Print delimiter for separation
+//         cout << "~" << "\n";
+//     }
+// }
+// // } Driver Code Ends
+
+
