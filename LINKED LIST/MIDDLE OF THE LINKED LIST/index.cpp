614be200e28ea5c4e5720e793224a815a55fff2b
Rahul-mandal01 <rahulra16809999@gmail.com>
2026-06-11 08:51:55 +0530

    Implement solution for Middle of the Linked List (LeetCode 876) using slow and fast pointers

--- LINKED LIST/MIDDLE OF THE LINKED LIST/index.cpp
+++ LINKED LIST/MIDDLE OF THE LINKED LIST/index.cpp
@@ -47,21 +47,6 @@ public:
     }
 };
 
-For LeetCode 876 (**Middle of the Linked List**), the standard solution is:
-
-```cpp
-ListNode* middleNode(ListNode* head) {
-    ListNode* slow = head;
-    ListNode* fast = head;
-
-    while (fast != nullptr && fast->next != nullptr) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    return slow;
-}
-```
 
 // # Time Complexity (TC)
 
